629a71a1df9008727adf8294ba9ad6349ec1c85c
Elvis Ibarra <ielvisd@gmail.com>
2019-02-13 11:34:53 -0800

    Initial setup, Room & Player classes created.

--- src/room.py
+++ src/room.py
@@ -1,2 +1,12 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room: 
+	def __init__(self, name, prompt, items):
+		self.name = name
+		self.prompt = prompt
+		self.items = items
+
+	def addItem(self, i):
+		print(f"Picked up {self.items[i]}!")
+
